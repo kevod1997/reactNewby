ebe32fac9202d51b4333b0be40199da7220847f2
Kevin Defalco <kevindefalco@gmail.com>
2022-08-17 17:37:34 -0300

    agregando componentes

--- src/App.js
+++ src/App.js
@@ -2,16 +2,53 @@
 import keduro from '../src/keduro.JPG'
 import './App.css';
 import { ChakraProvider } from '@chakra-ui/react';
+import NavBar from './components/NavBar';
 
 function App() {
+
+  // let i = 1;
+  // i = i + 1;
+  // i++;
+  // if(i == 0){
+  //   console.log('i es 0');
+  // }else{
+  //   console.log('i no es 0');
+  // }
+
+  //OPERADOR TERNARIO
+  // i == 0 ? console.log('i es 0') : console.log('i no es 0');
+
+  // console.log(i);
+
+
+  // desestructuracion
+  // const persona = {id: 1, nombre: 'juan', edad: 30};
+
+  // const {nombre, id} = persona;
+  // console.log(nombre);
+  // console.log(id);
+
+  let rojo = { color: 'red'};
+  let verde = { bakcground: 'green'};
+  let miestilo;
+  if(1==1){
+    miestilo = rojo
+  }else{
+    miestilo = verde
+  }
+let x = "Sos un maestro";
   return (
-    <ChakraProvider>
-    <div className="App">
+    /* className="App"*/
+    <div style={{color: 'black'}}>
+      <div>
+        <NavBar />
       <img className='kevo' src={keduro}></img>
-      <h1>Hola Miguel!</h1>
+      <h1>Hola Miguel! {x} </h1>
       <p>Me comere a suertudo</p>
+      </div>
+      <div>Footer</div>
     </div>
-    </ChakraProvider>
+
   );
 }
 
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from "react";
+
+export default function NavBar() {
+    return <div>
+        <ul>
+            <li>
+                <a href="#">link1</a>
+            </li>
+            <li>
+                <a href="#">link2</a>
+            </li>
+            <li>
+                <a href="#">link3</a>
+            </li>
+            <li>
+                <a href="#">link4</a>
+            </li>
+        </ul>
+    </div>;
+    }
